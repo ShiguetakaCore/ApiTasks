df6056a0bdad0ac1d28ff45ed89383a0c9009ca2
Koji Shiguetaka <shiguetakacore@outlook.com>
2022-05-05 05:24:47 -0300

    Add project files.

---
+++ ApiTasks/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ApiTasks.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ApiTasks.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { 
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuider)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            optionsBuider.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+    }
+}
---
+++ ApiTasks/Models/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiTasks.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string DataInclusao { get; set; }
+    }
+}
